64055b9da737bbbf639af527f25e961da576ac1c
jtknopf <jonathan@hdadvisors.net>
2024-01-05 16:58:46 -0500

    finish vhtf dashboard

--- vha/vhtf/vhtf_dashboard/app.R
+++ vha/vhtf/vhtf_dashboard/app.R
@@ -1,53 +1,40 @@
 ## Setup --------------------
 
-setwd("~/repos/sandbox/vha/vhtf")
+#setwd("~/repos/sandbox/vha/vhtf")
 
-source("vhtf.R")
+source("setup.R")
 
-library(shiny)
-library(leaflet)
-library(htmlwidgets)
-library(htmltools)
-library(DT)
-library(sf)
-library(shinyjs)
+## Load data ----------------
 
-vhtf <- read_rds("vhtf-districts.rds") |> 
-  st_transform(4326)
+vhtf <- read_rds("vhtf-districts.rds")
 
 districts <- read_rds("districts.rds")
 
-vha_pal <- colorFactor(c("#0C4D4F", "#19787B"), c("Senate", "House"))
-
-## Map ----------------------
-
-leaflet(data = filter(districts, district == "HD 1")) |> 
-  addProviderTiles(providers$CartoDB.Positron) |> 
-  addPolygons(
-    fillColor = ~vha_pal(chamber),
-    fillOpacity = 0.5,
-    color = ~vha_pal(chamber),
-    weight = 1.5,
-    #label = ~district,
-    #labelOptions = labelOptions(permanent = TRUE, textOnly = TRUE)
-  ) |> 
-  addMarkers(
-    data = vhtf,
-    popup = ~popup
-  ) |> 
-  fitBounds(~min(min_x), ~min(min_y), ~max(max_x), ~max(max_y))
-
 ## User interface -----------
 
-ui <- fluidPage(
+ui <- fixedPage(
   
+  # Load shinyjs to enable "Reset selection" button
   useShinyjs(),
   
-  titlePanel("VHTF Competitive Loan Pool Projects"),
+  # Load custom CSS
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ),
   
+  # App title
+  titlePanel("Virginia Housing Trust Fund project dashboard"),
+  
+  #p("Some test text to go here"),
+  
+  # Control panel
   wellPanel(
   
     fluidRow(
+      column(7,
+        markdown("**Competitive Loan Pool projects** (FY2014 - FY2023)"),
+        markdown("Use these controls to view projects by individual House of Delegates or State Senate district. You can also filter projects by clicking on a district in the map. Click the \"Reset selection\" button to reload all data.")
+      ),
       column(2,
         # Chamber filter
         radioButtons(
@@ -58,23 +45,39 @@ ui <- fluidPage(
         )
       ),
       column(3,
+        # District selection
         selectInput(
           "filter_district",
-          "Choose district:",
+          "Select district:",
           choices = NULL
-        )
-      ),
-      column(2,
-        actionButton("reset", "Reset selection"),
-        offset = 5
+        ),
+        # Reset selection button
+        actionButton("reset", "Reset selection")
       )
     )
   
   ),
   
   fluidRow(
-    column(12,
-      leafletOutput("map")
+    # Map
+    leafletOutput("map"),
+    br()
+  ),
+  
+  fluidRow(
+    # Tables
+    tabsetPanel(
+      tabPanel("Summary", dataTableOutput("summary")),
+      tabPanel("List of projects", dataTableOutput("list"))
+    )
+  ),
+  
+  fluidRow(
+    br(),
+    wellPanel(
+      markdown(
+        "**Source:** Virginia Department of Housing and Community Development. Data includes all projects awarded funds from the Competitive Loan Pool of the Virginia Housing Trust Fund, from FY2014 to FY2023.<br>
+      **Last updated:** January 5, 2024.")
     )
   )
   
@@ -84,6 +87,7 @@ ui <- fluidPage(
 
 server <- function(input, output, session){
   
+  # Reactive action to filter districts by chamber
   filtered_districts <- reactive({
     if (input$filter_chamber == "House") {
       filter(districts, chamber == "House")
@@ -92,14 +96,16 @@ server <- function(input, output, session){
     }
   })
   
+  # Reactive action to filter points by district
   filtered_points <- reactive({
     if (input$filter_district == "All districts") {
       vhtf
     } else {
-      filter(vhtf, if_all(c(house, senate), ~ .x == input$filter_district))
+      vhtf[vhtf$house == input$filter_district | vhtf$senate  == input$filter_district, ]
     }
   })
   
+  # Update district selection based on chamber filter
   observe({
     updateSelectInput(
       session,
@@ -108,7 +114,7 @@ server <- function(input, output, session){
       )
   })
   
-
+  # Default map
   output$map <- renderLeaflet({
     
     data <- filtered_districts()
@@ -119,6 +125,10 @@ server <- function(input, output, session){
     
     points <- filtered_points()
     
+    if(input$filter_district != "All districts") {
+      points <- points[points$house == input$filter_district | points$senate  == input$filter_district, ]
+    }
+    
     leaflet(options = leafletOptions(zoomControl = FALSE)) |> 
       addProviderTiles(providers$CartoDB.Positron) |>
       addPolygons(
@@ -126,10 +136,12 @@ server <- function(input, output, session){
         fillOpacity = 0.5,
         color = ~vha_pal(chamber),
         weight = 1.5,
-        layerId = ~district
+        layerId = ~district,
+        label = ~paste(district, " ")
       ) |> 
       addMarkers(
         data = points,
+        label = ~paste(project_name, " "),
         popup = ~popup
       ) |> 
       onRender(
@@ -140,6 +152,63 @@ server <- function(input, output, session){
     
   })
   
+  # Summary
+  output$summary <- renderDT({
+    
+    points <- filtered_points()
+    
+    if(input$filter_district != "All districts") {
+      points <- points[points$house == input$filter_district | points$senate  == input$filter_district, ]
+    }
+    
+    DT::datatable(
+      points %>% 
+        as.data.frame() %>% 
+        summarise(
+          n = n(),
+          units = sum(units_affordable),
+          amt = sum(amt_vhtf)
+        ), 
+      options = list(
+        dom = "t",
+        columnDefs = list(list(className = "dt-center", targets = "_all"))
+      ),
+      rownames = FALSE,
+      colnames = c("Projects awarded", "Affordable units produced", "Amount awarded"),
+      selection = "single"
+    ) |> 
+      formatRound(2, mark = ",", digits = 0) |> 
+      formatCurrency(3, digits = 0)
+    
+  })
+  
+  # List of projects
+  output$list <- renderDT({
+    
+    points <- filtered_points()
+    
+    if(input$filter_district != "All districts") {
+      points <- points[points$house == input$filter_district | points$senate  == input$filter_district, ]
+    }
+    
+    DT::datatable(
+      points %>% 
+        as.data.frame() %>% 
+        select(26, 27, 1, 4, 7, 15),
+      options = list(
+        dom = "tp",
+        order = list(3, 'asc'),
+        pageLength = 5
+      ),
+      rownames = FALSE,
+      colnames = c("HD", "SD", "Award year", "Project", "Affordable units", "VHTF award"),
+      selection = "single"
+    ) |> 
+      formatCurrency(6, digits = 0)
+    
+  })
+  
+  # Click to filter district polygon
   observeEvent(input$map_shape_click, {
     click <- input$map_shape_click
     if(!is.null(click$id)) {
@@ -156,7 +225,7 @@ server <- function(input, output, session){
       specificDistrict <- filtered[filtered$district == input$filter_district, ]
       
       points2 <- filtered_points()
-      specificPoints <- filter(points2, if_all(c(house, senate), ~ .x == input$filter_district))
+      specificPoints <- points2[points2$house == input$filter_district | points2$senate  == input$filter_district, ]
       
       leafletProxy("map") |> 
         clearShapes() |> 
@@ -166,10 +235,15 @@ server <- function(input, output, session){
           fillOpacity = 0.5,
           color = ~vha_pal(chamber),
           weight = 1.5,
+          label = ~paste(district, " "),
           layerId = ~district
         ) |> 
-        addMarkers(data = specificPoints)
-      
+        addMarkers(
+          data = specificPoints,
+          label = ~paste(project_name, " "),
+          popup = ~popup
+          )
+
     } else {
       
       leafletProxy("map") |> 
@@ -179,9 +253,14 @@ server <- function(input, output, session){
           fillOpacity = 0.5,
           color = ~vha_pal(chamber),
           weight = 1.5,
+          label = ~paste(district, " "),
           layerId = ~district
         ) |> 
-        addMarkers(data = filtered_points())
+        addMarkers(
+          data = filtered_points(),
+          label = ~paste(project_name, " "),
+          popup = ~popup
+          )
       
     }
   })
---
+++ vha/vhtf/vhtf_dashboard/setup.R
@@ -0,0 +1,191 @@
+
+## Setup ------------------------------
+
+# setwd("~/repos/sandbox/vha/vhtf")
+
+library(tidyverse)
+library(shiny)
+library(leaflet)
+library(htmlwidgets)
+library(htmltools)
+library(DT)
+library(sf)
+library(shinyjs)
+
+## VHA brand --------------------------
+
+# Font: Montserrat
+
+# Light green:  #A0D18E
+# Dark turq:    #0C4D4F
+# Yellow:       #ECC51E
+# Grey:         #2E3030
+# Light turq:   #19787B
+# Light blue:   #E3F3F5
+
+sysfonts::font_add_google("Montserrat", "Monsterrat", bold.wt = 600)
+
+vha <- c("#0C4D4F", "#A0D18E", "#ECC51E", "#19787B", "#2E3030", "#E3F3F5")
+
+## VHA ggplot theme -------------------
+
+theme_vha <- function(
+    base_size = 13,
+    base_family = "Montserrat",
+    flip_gridlines = FALSE
+) {
+  
+  ggplot2::theme_minimal() %+replace%
+    
+    ggplot2::theme(
+      
+      rect = ggplot2::element_rect(
+        fill = "white",
+        colour = "black",
+        size = 0.5,
+        linetype = 1L
+      ),
+      
+      text = ggplot2::element_text(
+        family = base_family,
+        face = "plain",
+        size = base_size,
+        colour = "#383c3d",
+        hjust = 0.5,
+        vjust = 0.5,
+        angle = 0,
+        lineheight = 0.9,
+        margin = ggplot2::margin(),
+        debug = FALSE
+      ),
+      
+      line = ggplot2::element_line(colour = "#000000",
+                                   size = 1,
+                                   linetype = 1L,
+                                   lineend = "butt"),
+      
+      plot.title = ggtext::element_textbox_simple(
+        size = base_size * 1.25,
+        color = "#383c3d",
+        hjust = 0L,
+        vjust = 0L,
+        margin = ggplot2::margin(b = 10, unit = "pt"),
+        face = "bold",
+        family = "Montserrat"
+      ),
+      
+      plot.subtitle = ggtext::element_textbox_simple(
+        size = base_size,
+        color = "#383c3d",
+        hjust = 0L,
+        vjust = 0L,
+        margin = ggplot2::margin(t = -5, b = 10, unit = "pt"),
+        face = "plain",
+        family = "Montserrat"
+      ),
+      
+      plot.caption = ggtext::element_textbox_simple(
+        size = base_size * 0.875,
+        color = "#383c3d",
+        hjust = 0L,
+        vjust = 0L,
+        margin = ggplot2::margin(t = 10, unit = "pt"),
+        lineheight = 1.1,
+        face = "plain",
+        family = "Montserrat"
+      ),
+      
+      plot.title.position = "plot",
+      
+      plot.caption.position = "plot",
+      
+      panel.background = ggplot2::element_blank(),
+      
+      panel.grid.minor = ggplot2::element_blank(),
+      
+      legend.position = "none",
+      
+      legend.title = ggplot2::element_blank(),
+      
+      axis.title = ggplot2::element_blank(),
+      
+      axis.ticks = ggplot2::element_blank()
+      
+    ) +
+    
+    if(flip_gridlines == FALSE) {
+      
+      ggplot2::theme(
+        
+        panel.grid.major.x = ggplot2::element_blank(),
+        
+        panel.grid.major.y = ggplot2::element_line(
+          color = "#cbcdcc",
+          size = 0.05
+        )
+        
+      )
+      
+    } else {
+      
+      ggplot2::theme(
+        
+        panel.grid.major.y = ggplot2::element_blank(),
+        
+        panel.grid.major.x = ggplot2::element_line(
+          color = "#cbcdcc",
+          size = 0.05
+        )
+        
+      )
+      
+    }
+  
+}
+
+## VHA color scales -------------------
+
+vha_pal <- colorFactor(c("#0C4D4F", "#19787B"), c("Senate", "House"))
+
+vha_pal_discrete <- function(direction = 1, repeat_pal = FALSE) {
+  
+  pal <- vha
+  
+  function(n) {
+    
+    if (repeat_pal) {
+      if (n > length(pal)) {
+        times <- ceiling(n / length(pal))
+        pal <- rep(pal, times)
+      }
+    }
+    
+    pal_n <- pal[1:n]
+    
+    if (direction == -1) {
+      return(rev(pal_n))
+    } else {
+      return(pal_n)
+    }
+    
+  }
+  
+}
+
+scale_color_vha <- function(direction = 1, repeat_pal = FALSE, ...) {
+  ggplot2::discrete_scale(
+    "colour", "vha", palette = vha_pal_discrete(direction = direction,
+                                                repeat_pal = repeat_pal),
+    ...
+  )
+}
+
+scale_fill_vha <- function(direction = 1, repeat_pal = FALSE, ...) {
+  ggplot2::discrete_scale(
+    "fill", "vha", palette = vha_pal_discrete(direction = direction,
+                                                repeat_pal = repeat_pal),
+    ...
+  )
+}
+
+
